07739191fae0801ab491080f8284971b7542768b
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2022-11-10 00:21:00 -0300

    Renewed UI

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,19 +1,19 @@
-var ctx = document.getElementById("myChart").getContext('2d');
+var ctx = document.getElementById("attendanceChart").getContext('2d');
 var att = [attendance['attended'], attendance['justified'], attendance['unjustified']];
 var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: ["Días Asistidos", "Días no asistidos justificados", "Días no asistidos no justificados"],
-      datasets: [{
-        label: "Population (millions)",
-        backgroundColor: ["#3cba9f", "#3e95cd", "#FF0000"],
-        data: att,
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Asistencia'
-      }
+  type: 'pie',
+  data: {
+    labels: ["Días Asistidos", "Días no asistidos justificados", "Días no asistidos no justificados"],
+    datasets: [{
+      label: "Attendance",
+      backgroundColor: ["#3cba9f", "#3e95cd", "#FF0000"],
+      data: att,
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Asistencia'
     }
+  }
 });
