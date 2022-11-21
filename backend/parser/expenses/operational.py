d3f734c1a9f2a9f60b5c037d9a6e075e9bbc34f7
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2022-11-21 18:17:42 -0300

    Cleaner requirements

--- backend/parser/expenses/operational.py
+++ backend/parser/expenses/operational.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
@@ -10,18 +9,19 @@ MONTHS = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agost
 # TODO: Add support for multiple years
 def get_deputy_operational_expenses(deputy_id, driver):
   """ Returns a dictionary with the expenses of a deputy in the current year """
-
+  # Page with Operational Expenses data
   url = f'https://www.camara.cl/diputados/detalle/gastosoperacionales.aspx?prmId={deputy_id}'
   month_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_DetallePlaceHolder_ddlMes'
   driver.get(url)
   expenses = dict()
   for month in MONTHS:
     try:
+      # Change the month in form
       month_selector = Select(driver.find_element(By.ID, month_selector_id))
-      month_selector.select_by_visible_text(month)
+      month_selector.select_by_visible_text(month) # Set corresponding month
       sleep(1) # wait for page to load
       month_expenses_table = driver.find_element(By.XPATH, '//*[@class="tabla"]').text
-      expenses[month] = parse_html_table(month_expenses_table)
+      expenses[month] = parse_html_table(month_expenses_table) # Obtain table with expenses
       print(f'Got expenses for {month}')
     except Exception as e:
       print(f'Could not find expenses for {month}')
@@ -38,9 +38,12 @@ def parse_html_table(table):
 
 def main(dep_id):
   options = webdriver.ChromeOptions()
+  # Location of Goofle Chrome binary
   options.binary_location = '/opt/google/chrome/google-chrome'
-  options.add_argument("--headless")
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+  options.add_argument("--headless") # We don't need a GUI
+  driver = webdriver.Chrome('chromedriver', options=options)
+
+  # Using the driver we obtain the expenses
   expenses = get_deputy_operational_expenses(dep_id, driver)
   driver.close()
   return expenses
