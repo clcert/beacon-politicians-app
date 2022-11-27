b6ee10a2c2e109f93ad2f40462718a9764c713e8
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2022-11-27 17:03:15 -0300

    Improve expenses format

--- backend/parser/expenses.py
+++ backend/parser/expenses.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
 
-MONTHS = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+from .utils import MONTHS, OP_EXPENSES_OFFICE, OP_EXPENSES_WEB, OP_EXPENSES_OTHERS, get_current_month, get_current_year
 
 class ExpensesParser:
 
@@ -25,25 +25,48 @@ class ExpensesParser:
     self.month_selector_id = ''
 
 
-  # TODO: Add support for multiple years
   def get_deputy_expenses(self):
     # Get the expenses page
     self.driver.get(self.url)
     expenses = []
-    for month in MONTHS:
+    change_year = False
+
+    # Get the current month and year
+    month_index = get_current_month()
+    year = get_current_year()
+    
+    while len(expenses) < 6:
+      if month_index == 0:
+        change_year = True
+        year -= 1
+      month_index = (month_index - 1) % 12
+      month = MONTHS[month_index]
       try:
-        # Change the month in form
+        # Change month in form
         month_selector = Select(self.driver.find_element(By.ID, self.month_selector_id))
         month_selector.select_by_visible_text(month) # Set corresponding month
+
+        if change_year: # Only change year if necessary
+          year_selector = Select(self.driver.find_element(By.ID, self.year_selector_id))
+          year_selector.select_by_visible_text(str(year)) # Set corresponding year
+          change_year = False
+
         sleep(1) # wait for page to load
+
         # Obtain table with expenses
         month_expenses_table = self.driver.find_element(By.XPATH, '//*[@class="tabla"]').text
         current_expenses = self.parse_and_filter_table(month_expenses_table) # Obtain table with expenses
+
         if current_expenses != None:
           current_expenses['month'] = month
+          current_expenses['year'] = year
           expenses.append(current_expenses)
+
       except Exception as e:
+        # print('Error parsing expenses for month: ' + month + ' and year: ' + str(year))
         continue
+
+    self.close_driver()
 
     return expenses
 
@@ -61,13 +84,28 @@ class OperationalExpensesParser(ExpensesParser):
     deputy_id = profile['deputy_id']
     self.url = f'https://www.camara.cl/diputados/detalle/gastosoperacionales.aspx?prmId={deputy_id}'
     self.month_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_DetallePlaceHolder_ddlMes'
+    self.year_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_DetallePlaceHolder_ddlAno'
 
   def parse_and_filter_table(self, html_table):
     lines = html_table.splitlines()
     expenses = dict()
+    expenses['Relacionados a Oficina Parlamentaria'] = 0
+    expenses['Web y Almacenamiento'] = 0
+    expenses['Otros'] = 0
+    total = 0
     for line in lines[1:]:
       [title, amount] = line.split('   ')
-      expenses[title] = int(amount.strip().replace('.', ''))
+      integer_amount = int(amount.strip().replace('.', ''))
+      total += integer_amount
+      if title in OP_EXPENSES_OFFICE:
+        expenses['Relacionados a Oficina Parlamentaria'] += integer_amount
+      elif title in OP_EXPENSES_WEB:
+        expenses['Web y Almacenamiento'] += integer_amount
+      elif title in OP_EXPENSES_OTHERS:
+        expenses['Otros'] += integer_amount
+      else:
+        expenses[title.lower().capitalize()] = integer_amount
+    expenses['total'] = total
     return expenses
 
 
@@ -78,19 +116,20 @@ class OfficesExpensesParser(ExpensesParser):
     self.deputy_name = f'{profile["first_surname"]} {profile["second_surname"][0]}., {profile["first_name"]}'
     self.url = 'https://www.camara.cl/transparencia/oficinasparlamentarias.aspx'
     self.month_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_ddlMes'
+    self.year_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_ddlAno'
 
   def parse_and_filter_table(self, html_table):
     lines = html_table.splitlines()
-    offices = []
+    total = 0
+    num_offices = 0
+
     for line in lines[1:]:
       [city, region, amount, deputy] = line.split('   ')
       if deputy.strip() == self.deputy_name:
-        offices.append({
-          'city': city.strip(),
-          'region': region.strip(),
-          'amount': int(amount.strip().replace('.', '')),
-        })
-    return { 'offices': offices } if len(offices) > 0 else None
+        num_offices += 1
+        total += int(amount.strip().replace('.', ''))
+
+    return { 'num_oficinas': num_offices, 'total': total } if num_offices > 0 else None
 
 
 class StaffExpensesParser(ExpensesParser):
@@ -100,16 +139,21 @@ class StaffExpensesParser(ExpensesParser):
     self.deputy_name = f'{profile["first_surname"]} {profile["second_surname"]}, {profile["first_name"]}'
     self.url = 'https://www.camara.cl/transparencia/personalapoyogral.aspx'
     self.month_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_ddlMes'
+    self.year_selector_id = 'ContentPlaceHolder1_ContentPlaceHolder1_ddlAno'
 
   def parse_and_filter_table(self, html_table):
     lines = html_table.splitlines()
-    staff = []
+    total = 0
+    staff_num = 0
+
     for line in lines[2:]:
       fields = line.split('   ')
       curr_dep_name = fields[0][2:].strip()
       if curr_dep_name == self.deputy_name:
-        staff.append({
-          'job': fields[2].strip(),
-          'amount': int(fields[3].strip().replace('.', '')),
-        })
-    return { 'staff' : staff } if len(staff) > 0 else None
+        staff_num += 1
+        if fields[1].strip() != '(*)' and fields[1].strip() != '(**)':
+          total += int(fields[3].strip().replace('.', ''))
+        else:
+          total += int(fields[4].strip().replace('.', ''))
+
+    return { 'num_personal': staff_num, 'total': total } if staff_num > 0 else None
