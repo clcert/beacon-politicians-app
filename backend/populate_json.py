a9b4e836b32daca08358e828a45f6646b877816d
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-01-31 20:09:35 -0300

    back: populate json in dockerfile.

--- backend/populate_json.py
+++ backend/populate_json.py
@@ -1,10 +1,14 @@
 from updater import Updater
 from datetime import datetime, timedelta
+from sys import argv
 
 updater = Updater()
 
+days = int(argv[1]) if len(argv) > 1 else 1
+days -= 1
+
 # Populates the json with the last 7 days
-for delta in range(6,-1,-1):
+for delta in range(days,-1,-1):
     date = (datetime.now() - timedelta(days=delta)).replace(hour=0, minute=0, second=0, microsecond=0)
     print(date)
     updater.update(using_json=True, date_hour=date)
