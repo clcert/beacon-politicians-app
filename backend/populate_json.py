0fb8ce8e1baa8db9a0d3acdd8c02cd31baade256
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-02-01 23:54:03 -0300

    back: fix populate script according to timezones

--- backend/populate_json.py
+++ backend/populate_json.py
@@ -4,11 +4,24 @@ from sys import argv
 
 updater = Updater()
 
-days = int(argv[1]) if len(argv) > 1 else 1
-days -= 1
+days_to_calculate = int(argv[1]) if len(argv) > 1 else 1
+days_to_calculate = min(days_to_calculate, 7)
+
+dt_utc = datetime.utcnow()
+dt_local = datetime.now()
+
+consider_today = dt_utc.day > dt_local.day or (
+    dt_utc.day == dt_local.day and dt_utc.hour > 3
+)
+
+if consider_today:
+    initial_date = (datetime.now() - timedelta(days=days_to_calculate-1))
+else:
+    initial_date = (datetime.now() - timedelta(days=days_to_calculate))
+
+initial_date = initial_date.replace(hour=0, minute=0, second=0, microsecond=0)
 
 # Populates the json with the last 7 days
-for delta in range(days,-1,-1):
-    date = (datetime.now() - timedelta(days=delta)).replace(hour=0, minute=0, second=0, microsecond=0)
-    print(date)
+for delta in range(days_to_calculate):
+    date = (initial_date + timedelta(days=delta))
     updater.update(using_json=True, date_hour=date)
