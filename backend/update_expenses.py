c30f9f8fb6900b86293d3e2ed8f806fed09bac12
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-04-01 15:26:38 -0300

    back: save average expenses in file

--- backend/update_expenses.py
+++ backend/update_expenses.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from datetime import datetime
 from time import perf_counter
+from statistics import mean, stdev
 
 from threading import Thread, Lock
 
@@ -18,7 +19,7 @@ from deputies.parsers.expenses import (
 )
 
 # settings
-from settings import JsonFiles, FILE_LOCATIONS
+from settings import JsonFiles, FILE_LOCATIONS, OP_EXPENSES_TYPES
 
 import json
 
@@ -62,6 +63,10 @@ def parse_expenses(profile, driver):
 
 
 def get_driver():
+    """
+    Function that returns a driver to be used in the parsing process.
+    :return: Returns a driver.
+    """
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--headless") # We don't need a GUI
@@ -74,11 +79,18 @@ def get_driver():
 
 
 def create_file():
+    """
+    Function that creates the expenses file if it doesn't exist.
+    """
     with open(FILE_LOCATIONS[JsonFiles.EXPENSES], 'w') as f:
         f.write('')
     
 
 def save_deputy_expenses(deputy_profile):
+    """
+    Function that saves the expenses of a deputy in the expenses file.
+    :param deputy_profile: Dictionary containing the expenses of a deputy.
+    """
     data = get_json_data(JsonFiles.EXPENSES)
     if data is None:
         data = {'expenses': [], 'last_update': ''}
@@ -97,6 +109,11 @@ def save_deputy_expenses(deputy_profile):
 
 
 def parse_deputies_expenses(writer_lock, indexes_list):
+    """
+    Function that parses the expenses of a list of deputies.
+    :param writer_lock: Lock to be used when writing to the expenses file.
+    :param indexes_list: List of indexes of the deputies to be parsed.
+    """
     driver = get_driver()
 
     if driver is None:
@@ -114,12 +131,101 @@ def parse_deputies_expenses(writer_lock, indexes_list):
     
     driver.close()
 
+def get_month_data(deputies_expenses, expense_type, month):
+    """
+    Function that gets the expenses of a given month and expense type.
+    :param deputies_expenses: List of dictionaries containing the expenses of each deputy.
+    :param expense_type: Type of expenses to be filtered.
+    :param month: Month to be filtered.
+    :return: Returns a list of dictionaries containing the expenses of each deputy.
+    """
+    # get deputies expenses data
+    deputies_data = map(
+        lambda deputy: deputy['expenses'][expense_type],
+        deputies_expenses
+    )
+    # filter by month
+    month_data = map(
+        lambda deputy_operational: 
+            next(filter(
+                lambda deputy_operational_month: deputy_operational_month['month'] == month,
+                deputy_operational,
+            )),
+        deputies_data,
+    )
+    return list(month_data)
 
-def sort_expenses_by_deputy_index():
+
+def get_operational_detail(month_data):
+    """
+    Function that gets the operational detail of a given month.
+    :param month_data: List of dictionaries containing the expenses of each deputy.
+    :return: Returns a dictionary containing the average expenses of each category.
+    """
+    month_operational_detail = {}
+
+    for category in OP_EXPENSES_TYPES:
+        total_avg = round(mean(map(lambda x: x[category], month_data)))
+        total_std = round(stdev(map(lambda x: x[category], month_data)))
+        month_operational_detail[category] = {
+            'total': total_avg,
+            'total_std': total_std,
+        }
+
+    return month_operational_detail
+
+def compute_average_expenses(deputies_expenses):
+    """
+    Computes the average expenses of all deputies.
+    :param expenses: List of dictionaries containing the expenses of each deputy.
+    :return: Dictionary containing the average expenses of each category.
+    """
+
+    average_expenses_dict = {
+        'operational': {},
+        'offices': {},
+        'staff': {},
+    }
+
+    first_deputy_expenses = deputies_expenses[0]['expenses']
+    months = {
+        'operational': list(map(lambda x: x['month'], first_deputy_expenses['operational'])),
+        'offices': list(map(lambda x: x['month'], first_deputy_expenses['offices'])),
+        'staff': list(map(lambda x: x['month'], first_deputy_expenses['staff'])),
+    }
+
+    for expense_type in average_expenses_dict.keys():
+        average_expenses_dict[expense_type] = []
+
+        for month in months[expense_type]:
+            month_data = get_month_data(deputies_expenses, expense_type, month)
+
+            month_operational_detail = None
+            if expense_type == 'operational':
+                month_operational_detail = get_operational_detail(month_data)
+
+            total_avg = round(mean(map(lambda x: x['total'], month_data)))
+            total_std = round(stdev(map(lambda x: x['total'], month_data)))
+
+            average_expenses_dict[expense_type].append({
+                'total': total_avg,
+                'total_std': total_std,
+                'month': month,
+                'detail': month_operational_detail,
+            })
+
+    return average_expenses_dict
+
+
+def add_avg_and_sort():
+    """
+    Function that adds the average expenses to the expenses file and sorts the expenses by index.
+    """
     data = get_json_data(JsonFiles.EXPENSES)
     expenses = data['expenses']
     sorted_expenses = sorted(expenses, key=lambda x: x['index'])
     data['expenses'] = sorted_expenses
+    data['average_expenses'] = compute_average_expenses(sorted_expenses)
 
     file_path = FILE_LOCATIONS[JsonFiles.EXPENSES]
 
@@ -133,8 +239,7 @@ if __name__ == '__main__':
 
     create_file()
 
-    # create a shared lock
-    writer_lock = Lock()
+    writer_lock = Lock() # shared lock
 
     threads_list = []
     for i in range(THREADS):
@@ -145,4 +250,4 @@ if __name__ == '__main__':
     for thread in threads_list:
         thread.join()
 
-    sort_expenses_by_deputy_index()
+    add_avg_and_sort()
