a6957807f5e059ab6125567d899658524348c888
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-04-17 12:06:19 -0400

    front: add votings style

--- frontend/components/DeputyVotings.js
+++ frontend/components/DeputyVotings.js
@@ -10,21 +10,81 @@ const DeputyVotings = ({data}) => {
       { 
         data.voting.map((vote, index) => {
           return (
-            <section key={index}>
-              <header>
-                <h3>{ vote.description }</h3>
-                <p>{ vote.name }</p>
-              </header>
-              { 
-                vote.article !== '' && 
-                vote.article !== 'No se encontró el artículo' && 
-                <p>{vote.article}</p>
-              }
-            </section>
+            <SingleVote key={index} vote={vote} sex={data.sex} />
+            // <section key={index}>
+            //   <header>
+            //     <h3>{ vote.description }</h3>
+            //     <p>{ vote.name }</p>
+            //   </header>
+            //   { 
+            //     vote.article !== '' && 
+            //     vote.article !== 'No se encontró el artículo' && 
+            //     <p>{vote.article}</p>
+            //   }
+            // </section>
           )
         })
       }
     </>
+  )
+}
+
+const SingleVote = ({vote, sex}) => {
+  return (
+    <div className='vote-container'>
+      <div className='head'>
+        <h3>{ vote.description }</h3>
+        <div className='date'><i>{ vote.date }</i></div>
+      </div>
+      
+      <div className='matters'>{ vote.name }</div>
+      { 
+        vote.article !== '' && 
+        vote.article !== 'No se encontró el artículo' && 
+        <p>{vote.article}</p>
+      }
+      <span className='vote'>
+        {
+          sex === '0' ?
+            'La diputada votó '
+            :
+            'El diputado votó '
+        }
+      </span>
+      {
+        vote.vote_option === 'Afirmativo' ?
+          <span className='vote green-vote'>{ vote.vote_option }</span>
+          :
+        vote.vote_option === 'En Contra' ?
+          <div className='vote red-vote'>{ vote.vote_option }</div>
+          :
+          <div className='vote'>{ vote.vote_option }</div>
+      }
+      <div className='more-info'>
+        <div className='votes-info'>
+          <b style={{'color': 'gray'}}>Resultado Votación</b>
+          <br/>
+          <b style={{'color': 'limegreen'}}>{ vote.total_yes }</b> &nbsp;
+          <b style={{'color': 'red'}}>{ vote.total_no }</b> &nbsp;
+          <b style={{'color': 'gray'}}>{ vote.total_abstention }</b>&nbsp;
+          <br/>
+          {
+            vote.result === 'Aprobado' ?
+              <b className='vote-count green-vote'>{ vote.result }</b>
+              :
+            vote.result === 'Unánime' ?
+              <b className='vote-count red-vote'>{ vote.result }</b>
+              :
+              <b className='vote-count'>{ vote.result }</b>
+          }
+        </div>
+        <div className='link-info'>
+          <a href={`https://www.camara.cl/legislacion/sala_sesiones/votacion_detalle.aspx?prmIdVotacion=${vote.voting_id}`} target='_blank' rel="noreferrer">
+            Ver detalle
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
