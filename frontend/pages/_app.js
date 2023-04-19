54338f2bd7859f9a766c722b87010d89f6d3de3e
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-04-18 23:53:40 -0400

    front: delete old files

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
 
 // Styles
 import '../public/assets/css/template_main.css';
@@ -7,29 +6,6 @@ import '../public/assets/css/custom.css';
 import '../public/assets/css/spinner.css';
 
 function MyApp({ Component, pageProps }) {
-
-  // Add external scripts
-  useEffect(() => {
-
-    function add_script (source) {
-      const script = document.createElement('script');
-      script.src = source;
-      script.async = true;
-      document.body.appendChild(script);
-      return script;
-    }
-
-    const scripts = [
-      add_script('assets/js/jquery.min.js'),
-      // add_script('assets/js/browser.min.js'),
-      // add_script('assets/js/breakpoints.min.js'),
-      // add_script('assets/js/util.js'),
-      // add_script('assets/js/main.js'),
-    ]
-    
-    return () => scripts.forEach(script => document.body.removeChild(script));
-  }, []);
-
   return (
     <div>
       <Head>
