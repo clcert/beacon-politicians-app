c6d79a3145892c19495da581f6a7cf6681fe59a7
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-06-12 13:06:14 -0400

    Show age instead of birthday.

--- frontend/components/DeputyOverview.js
+++ frontend/components/DeputyOverview.js
@@ -2,27 +2,40 @@ import React from 'react'
 
 const DeputyOverview = ({data}) => {
   const deputyName = `${ data.first_name } ${ data.first_surname }`
+  const selectionDate = new Date(data.date);
+  const bornDate = new Date(data.birthday);
+  const selectionDateStr = selectionDate.toLocaleDateString('es-ES', {
+    day: "numeric", 
+    month: "long",
+    timeZone: "America/Santiago"
+  });
+  const isMale = data.sex === '1';
+  const deputyTitle = isMale ? 'Diputado' : 'Diputada';
+  const titleWithPronoun = isMale ? 'El diputado' : 'La diputada';
+  const shortName = `${ data.first_name } ${ data.first_surname }`;
+  const fullName = `${ data.first_name } ${ data.second_name } ${ data.first_surname } ${ data.second_surname }`;
+  const age = new Date().getFullYear() - bornDate.getFullYear();
+  const lastPeriod = data.lastperiod.split('-');
 
   return (
     <>
       <a href={data.photo} target='_blank' rel="noreferrer" className='image deputy-pic'>
         <img src={data.photo} alt={`Diputad${ data.termination } ${ deputyName }`} />
       </a>
-      <h1>Diputad{ data.termination } <strong>{ data.first_name } { data.first_surname }</strong></h1>
+      <h1>{ deputyTitle } <strong>{ shortName }</strong></h1>
       <h2>{ data.party }</h2>
       <div className='deputy-overview'>
         <p>
-          {data.sex === '1' ? 'El diputado' : 'La diputada'} del día <strong>{data.date.split(' ')[0]}</strong> es{' '}
-          <strong>{ data.first_name } { data.second_name } { data.first_surname } { data.second_surname }</strong>,{' '}
-          nacid{ data.termination } el <strong>{ data.birthday }</strong>,{' '}
+          { titleWithPronoun } del día <strong>{ selectionDateStr }</strong> es{' '}
+          <strong>{ fullName }</strong> con <strong>{ age } años</strong> de edad
           {
             data.profession === 'Sin Información' ? '' : (
-              <>es de profesión <strong>{ data.profession }</strong>, </>
+              <> y es de profesión <strong>{ data.profession }</strong>.</>
             )
           }
-          quien es actualmente diputad{ data.termination } por el <strong>distrito { data.district }</strong>{' '}
+          . Actualmente es { deputyTitle.toLocaleLowerCase() } por el <strong>distrito { data.district }</strong>{' '}
           que forma parte de la <strong>{ data.districtregion }</strong>,{' '}
-          durante el período { data.lastperiod }.
+          durante el período { lastPeriod[0] } a { lastPeriod[1] }.
         </p>
         <hr />
         <div className='parlamentary-periods'>
