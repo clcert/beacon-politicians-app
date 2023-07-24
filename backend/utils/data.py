6a72768c77f9ccd742acfecaac4a3a700ca8c0e3
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-07-24 11:19:45 -0400

    Update path to db files in settings

--- backend/utils/data.py
+++ backend/utils/data.py
@@ -1,6 +1,6 @@
-from pathlib import Path
+import os
 
-PROJECT_DIR = str(Path().absolute())
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).replace('/utils', '')
 
 OPENDATA_CAMARA_URL = 'http://opendata.camara.cl/camaradiputados/WServices/'
 CURRENT_DEPUTIES_URL = OPENDATA_CAMARA_URL + 'WSDiputado.asmx/retornarDiputadosPeriodoActual'
